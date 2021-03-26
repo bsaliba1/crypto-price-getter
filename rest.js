ac952234b3f204dfc6335188f7de47df292e4c79
bsaliba <baptsal@gmail.com>
2021-03-26 16:52:45 -0400

    Creating class to properly send requests to Coinbase API

--- rest.js
+++ rest.js
@@ -1,26 +1,66 @@
 const https = require('https');
 
-let options = {
+let API_SERVER = {
   method: 'GET',
   hostname: 'api.pro.coinbase.com',
-  path: '/products',
-  port: 443,
+  path: '/products/BTC-USD/stats',
+  port: 443
 }
 
-const req = https.request(options, res => {
-  // console.log(`statusCode: ${res.statusCode}`)
+class PriceGetter {
+  static get24hrPrice(asset, base){
 
-  let response = ""
-  res.on('data', d => {
-    response += d.toString()
-  })
-  res.on('end', () => {
-    console.log(response)
-  })
-})
+  }
 
-req.on('error', error => {
-  console.error(error)
-})
+  static getLatestPrice(asset, base){
 
-req.end()
+  }
+
+  static compare24hrPrice(asset, base){
+
+  }
+
+  // Private
+
+  // Sends HTTP request to the Coinbase REST API
+  // Return value is the requested information in JSON form
+  // If there is an error the return value is 'undefined'
+  static sendRequest(options){
+    const request_options = this.formatRequest(options)
+    const request = https.request(API_SERVER, response => {
+      if (this.invalidStatusCode(response)){
+        console.log(`Status code: ${response.statusCode} \nCould not retrieve data`)
+        return undefined
+      }
+
+      let data = ""
+      response.on('data', dataChunk => {
+          data += dataChunk.toString()
+      })
+      response.on('end', () => {
+        console.log(data)
+      })
+    })
+
+    request.on('error', error => {
+      console.error(error)
+      return undefined
+    })
+
+    request.end()
+  }
+
+  static formatRequest(options) {
+    // TODO
+  }
+
+  static invalidStatusCode(response){
+    const statusCode = parseInt(response.statusCode)
+    if (!(200 <=  statusCode && statusCode <= 299)) {
+      return true
+    }
+    return false
+  }
+}
+
+PriceGetter.sendRequest()
