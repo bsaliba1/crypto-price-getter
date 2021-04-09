2e1adf26084ddce8d34d1c027c50e5f8beb7e3c9
bsaliba <baptsal@gmail.com>
2021-04-09 11:30:16 -0400

    Removing unneeded file

--- api_credentials.js
+++
@@ -1,9 +0,0 @@
-const apiCredentials = {
-  key: 'FILL ME IN',
-  secret: 'FILL ME IN',
-  passphrase: 'FILL ME IN'
-}
-
-exports.key = apiCredentials.key;
-exports.secret = apiCredentials.secret;
-exports.passphrase = apiCredentials.passphrase;
