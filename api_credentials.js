9ebb8effc6cafa5834f147c9327bd6ee40a8c69e
bsaliba <baptsal@gmail.com>
2021-04-09 09:55:32 -0400

    Adding file to insert test credentials

---
+++ api_credentials.js
@@ -0,0 +1,9 @@
+const apiCredentials = {
+  key: 'FILL ME IN',
+  secret: 'FILL ME IN',
+  passphrase: 'FILL ME IN'
+}
+
+exports.key = apiCredentials.key;
+exports.secret = apiCredentials.secret;
+exports.passphrase = apiCredentials.passphrase;
