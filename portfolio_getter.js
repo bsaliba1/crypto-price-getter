b0a041a8a7d2106477fd19be83446905caa07f5a
bsaliba <baptsal@gmail.com>
2021-04-09 11:42:26 -0400

    More refactors

--- portfolio_getter.js
+++ portfolio_getter.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const axios = require('axios');
-const apiCredentials = require('./default_api_credentials');
+const defaultApiCredentials = require('./default_api_credentials');
 
 const requestOptions = {
   method: 'GET',
@@ -8,40 +8,41 @@ const requestOptions = {
   url: 'https://api.pro.coinbase.com'
 }
 
-async function getPortfolio(key = apiCredentials.key, secret = apiCredentials.secret, passphrase = apiCredentials.passphrase){
-  requestOptions.key = key;
-  requestOptions.secret = secret;
-  requestOptions.passphrase = passphrase;
-  const res = await sendRequest();
-  return res
+async function getPortfolio(key = defaultApiCredentials.key, secret = defaultApiCredentials.secret, passphrase = defaultApiCredentials.passphrase){
+  let options = {...requestOptions}
+  options.key = key;
+  options.secret = secret;
+  options.passphrase = passphrase;
+  const response = await sendRequest(options);
+  return response
 }
 
 /* PRIVATE */
 //
 //
 // Sends a formatted request to coinbase using the given API key, secret, and passphrase
-async function sendRequest(){
-  const request = formatRequest();
-  let res = ""
+async function sendRequest(options){
+  const request = formatRequest(options);
+  let response = ""
   try {
-    res = await axios.request(request);
+    response = await axios.request(request);
   } catch(error) {
     console.error(`Request Error: ${error.response.data.message}`);
     return
   }
-  return res.data
+  return response.data
 }
 
 // Sends a formatted request using the given API key, secret, and passphrase
-function formatRequest(){
+function formatRequest(options){
   const method = 'GET';
-  const url = requestOptions.url + requestOptions.requestPath;
+  const url = options.url + options.requestPath;
   const timestamp = Date.now() / 1000;
   const headers = {
-    'CB-ACCESS-KEY': requestOptions.key,
-    'CB-ACCESS-SIGN': getSignature(timestamp),
+    'CB-ACCESS-KEY': options.key,
+    'CB-ACCESS-SIGN': getSignature(options, timestamp),
     'CB-ACCESS-TIMESTAMP': timestamp,
-    'CB-ACCESS-PASSPHRASE': requestOptions.passphrase,
+    'CB-ACCESS-PASSPHRASE': options.passphrase,
   }
   const formattedRequest = {
     method: method,
@@ -52,12 +53,12 @@ function formatRequest(){
 }
 
 // Creates a API signature using the given API key, secret, passphrase, and timestamp
-function getSignature(timestamp){
+function getSignature(options, timestamp){
   // create the prehash string by concatenating required parts
-  let prehashString = timestamp + requestOptions.method + requestOptions.requestPath;
+  let prehashString = timestamp + options.method + options.requestPath;
 
   // decode the base64 secret
-  const key = Buffer.from(requestOptions.secret, 'base64');
+  const key = Buffer.from(options.secret, 'base64');
 
   // create a sha256 hmac with the secret
   let hmac = crypto.createHmac('sha256', key);
